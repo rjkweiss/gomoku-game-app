2e56eb4353b8bd548c0c275b7317d25f0d045f94
Joyce Kimojino-Weiss <jkimojinoweiss@gmail.com>
2025-12-28 17:43:37 -0600

    Fix module resolution for Vercel serverless functions

--- api/users.ts
+++ api/users.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { prisma } from "../lib/prisma";
+import { prisma } from "../lib/prisma.js";
 
 const handler = async (req: VercelRequest, res: VercelResponse) => {
     try {
